6f08da5bef40f32b2eb0a99621d0aa75535421ee
ab5 fr <63975413+xXA2005@users.noreply.github.com>
2023-06-26 23:50:20 +0300

    Update server.py
    repl fix

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ load_dotenv()
 @app.route("/",methods=['POST',"GET"])
 def gogo():
     r = requests.post(os.getenv("WEBHOOK"),json=request.json)
+    # r = requests.post(os.environ["WEBHOOK"],json=request.json) # for repl use os.environ instead of getenv
     return r.text, r.status_code
     
 app.run(debug=True)
